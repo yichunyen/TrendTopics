78bd0cbdb33527ec1c79cac663f5bacc54efcce2
Yi-Chun <yenyichun.developer@gmail.com>
2026-02-11 14:32:05 +0800

    infra: indent

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -4,6 +4,6 @@ resource "google_storage_bucket" "tw_calendar_bucket" {
   project  = var.project_id
 
   uniform_bucket_level_access = true
-  public_access_prevention = "enforced"
-  depends_on = [google_project_service.required_apis]
+  public_access_prevention    = "enforced"
+  depends_on                  = [google_project_service.required_apis]
 }
